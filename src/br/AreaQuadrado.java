148c670f039677466f343fc73166e9d06e889188
brenoas1 <brenooasantos@gmail.com>
2025-03-21 06:33:50 -0300

    Exercicio 2 Fundamentos da linguagem de programação Java

---
+++ src/br/AreaQuadrado.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AreaQuadrado {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita ao usuário o tamanho do lado do quadrado
+        System.out.print("Digite o tamanho do lado do quadrado: ");
+        double lado = scanner.nextDouble();
+
+        // Calcula a área do quadrado
+        double area = lado * lado;
+
+        // Exibe o resultado
+        System.out.println("A área do quadrado é: " + area);
+
+        scanner.close();
+    }
+}
